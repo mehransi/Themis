9a1ed99395b71d139294cdd0fcbba30311520f20
mehransi <mehransi95@gmail.com>
2024-05-18 18:01:32 +0200

    Read model server port number from ENV

--- pipelines/video/classifier.py
+++ pipelines/video/classifier.py
@@ -90,4 +90,4 @@ app.add_routes(
 )
 
 if __name__ == '__main__':
-    web.run_app(app, host="0.0.0.0", port=8003, access_log=None)
+    web.run_app(app, host="0.0.0.0", port=int(os.getenv("PORT", 8003)), access_log=None)
